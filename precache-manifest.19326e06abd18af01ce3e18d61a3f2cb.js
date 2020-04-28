6c390c7eec865a53597169191640e56862ff7132
Vannisa Pascal-Nelson <vannisaepascalnelson@gmail.com>
2020-04-27 23:04:44 -0400

    Updates

---
+++ precache-manifest.19326e06abd18af01ce3e18d61a3f2cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe016e15a4af1c4bcdd517e10b0aa2c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6612af4e5fc2acb97e3b",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "afda21390b6b4cb282a8",
+    "url": "/portfolio/static/js/2.262eeec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.262eeec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6612af4e5fc2acb97e3b",
+    "url": "/portfolio/static/js/main.678f174f.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
